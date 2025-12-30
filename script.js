83f316d3de2b2776f51c60eda1229c7a406516d6
sqwizee <77798787+SharicW@users.noreply.github.com>
2025-12-30 16:38:07 +0400

    Refactor Discord auth and Firebase initialization

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const DISCORD_LOGIN_URL =
   "https://discord.com/oauth2/authorize" +
   "?client_id=1455371713276805344" +
-  "&redirect_uri=https://USERNAME.github.io/bulk-awards/" +
+  "&redirect_uri=https://sharicw.github.io/bulk-awards/" +
   "&response_type=token" +
   "&scope=identify";
 
@@ -335,4 +335,5 @@ function updateCountdown() {
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
 
